436078f45ea44333fa576b586b60a22866071543
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-11-20 23:19:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddi
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 
-
 from langchain.document_loaders import CSVLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import RetrievalQA
@@ -15,31 +14,94 @@ from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
 from langchain.chains import LLMMathChain
 
-
-loader = CSVLoader(file_path='data.csv')
-pages = loader.load()
-print("Number of pages loaded:", len(pages))  # Debugging print statement
-print(pages)
-
-# create calculator tool
-calculator = LLMMathChain.from_llm(llm=llm_hub, verbose=True)
-
 import sqlite3
-from flask import Flask, render_template, request, redirect, url_for
-
-app = Flask(__name__)
+from langchain.prompts import PromptTemplate
 
 def get_db_connection():
-    conn = sqlite3.connect('tracker.db')
+    conn = sqlite3.connect('expenses.db')
     conn.row_factory = sqlite3.Row
-    # Check if the table exists, and create it if it doesn't
-    conn.execute("""CREATE TABLE IF NOT EXISTS expense (
+    # Create the table with an auto-incrementing primary key
+    conn.execute("""CREATE TABLE IF NOT EXISTS expenses (
+        rowid INTEGER PRIMARY KEY AUTOINCREMENT,
         date DATE,
         item TEXT,
         amount INTEGER
     )""")
+    conn.commit()
     return conn
 
+"""
+Building LLM Connected To Our Database
+"""
+
+conn = sqlite3.connect('records.db')
+
+# Create a cursor object to execute SQL commands
+cursor = conn.cursor()
+
+# Create the tasks table if it doesn't exist
+conn.execute("""CREATE TABLE IF NOT EXISTS expenses (
+    rowid INTEGER PRIMARY KEY AUTOINCREMENT,
+    date DATE,
+    item TEXT,
+    amount INTEGER
+)""")
+
+# Insert sample tasks into the tasks table
+data = [
+    ('2023-11-11', 'Groceries', 150),
+    ('2023-11-13', 'Utilities', 75),
+    ('2023-11-15', 'Internet', 50),
+    ('2023-11-18', 'Transport', 30),
+    ('2023-11-20', 'Eating Out', 45),
+    ('2023-11-22', 'Groceries', 160),
+    ('2023-11-25', 'Gym Membership', 25),
+    ('2023-12-18', 'Utilities', 80),
+    ('2023-12-20', 'Clothing', 100),
+    ('2023-12-23', 'Entertainment', 60),
+    ('2023-12-25', 'Internet', 50),
+    ('2023-12-28', 'Groceries', 120),
+    ('2023-12-30', 'Phone Bill', 40)
+]
+# Ensure your database table structure matches these columns (date, item, amount)
+cursor.executemany('INSERT INTO expenses (date, item, amount) VALUES (?, ?, ?)', data)
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
+
+from langchain.utilities import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
+# connect to our database
+db = SQLDatabase.from_uri("sqlite:///records.db")
+
+# We can use db.table_info and check which sample rows are included:
+print(db.table_info)
+
+# create the database chain
+db_chain = SQLDatabaseChain.from_llm(llm_hub, db, verbose=True)
+
+# Create db chain
+QUERY = """
+Given an input question, first create a syntactically correct PostgreSQL query to run, then look at the results of the query and return the answer.
+Use the following format:
+
+Question: Question here
+SQLQuery: SQL Query to run
+SQLResult: Result of the SQLQuery
+Answer: Final answer here
+
+{question}
+"""
+
+question = "total amount?"
+prompt = QUERY.format(question=question)
+answer = db_chain.run(prompt)
+print(f"answer: ")
+
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -52,13 +114,13 @@ def index():
         amount = request.form['amount']
 
         # Insert new expense into the database
-        conn.execute('INSERT INTO expense (date, item, amount) VALUES (?, ?, ?)', (date, item, amount))
+        conn.execute('INSERT INTO expenses (date, item, amount) VALUES (?, ?, ?)', (date, item, amount))
         conn.commit()
         save_to_csv()  # Update CSV file after adding new expense
         return redirect(url_for('index'))
 
     # Fetch all expenses from the database
-    all_expenses = conn.execute('SELECT rowid, * FROM expense ORDER BY date DESC').fetchall()
+    all_expenses = conn.execute('SELECT rowid, * FROM expenses ORDER BY date DESC').fetchall()
     for expense in all_expenses:
         print(expense['rowid'])  # This should print the rowid of each expense
 
@@ -66,28 +128,27 @@ def index():
 
     return render_template('index.html', all_expenses=all_expenses)
 
-
-
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
     conn = get_db_connection()
-    expense = conn.execute('SELECT * FROM expense WHERE rowid = ?', (id,)).fetchone()
+    expense = conn.execute('SELECT * FROM expenses WHERE rowid = ?', (id,)).fetchone()
 
     if request.method == 'POST':
         date = request.form['date']
         item = request.form['item']
         amount = request.form['amount']
-        conn.execute('UPDATE expense SET date = ?, item = ?, amount = ? WHERE rowid = ?', (date, item, amount, id))
+        conn.execute('UPDATE expenses SET date = ?, item = ?, amount = ? WHERE rowid = ?', (date, item, amount, id))
         conn.commit()
         save_to_csv()
         return redirect(url_for('index'))
 
+    conn.close()
     return render_template('edit.html', expense=expense)
 
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete(id):
     conn = get_db_connection()
-    conn.execute('DELETE FROM expense WHERE rowid = ?', (id,))
+    conn.execute('DELETE FROM expenses WHERE rowid = ?', (id,))
     conn.commit()
     conn.close()
     return redirect(url_for('index'))
@@ -95,14 +156,14 @@ def delete(id):
 @app.route('/records')
 def records():
     conn = get_db_connection()
-    expenses = conn.execute('SELECT rowid, * FROM expense ORDER BY date DESC').fetchall()
+    expenses = conn.execute('SELECT rowid, * FROM expenses ORDER BY date DESC').fetchall()
 
     # SQL to calculate total amount spent per month
     totals_by_month = conn.execute("""
         SELECT 
             strftime('%Y-%m', date) as month, 
             sum(amount) as total 
-        FROM expense 
+        FROM expenses
         GROUP BY strftime('%Y-%m', date)
         ORDER BY month DESC
     """).fetchall()
@@ -111,119 +172,20 @@ def records():
     return render_template('records.html', expenses=expenses, totals_by_month=totals_by_month)
 
 
-import csv
-
-def save_to_csv():
-    conn = get_db_connection()
-    expenses = conn.execute('SELECT rowid, date, item, amount FROM expense ORDER BY date DESC').fetchall()
-    conn.close()
-
-    with open('data.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['ID', 'Date', 'Item', 'Amount'])
-        for expense in expenses:
-            writer.writerow([expense['rowid'], expense['date'], expense['item'], expense['amount']])
-
-from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
-from langchain.prompts import PromptTemplate
-import pandas as pd
-from langchain_experimental.tools.python.tool import PythonAstREPLTool
-
-template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer.
-{context}
-Question: {question}
-Helpful Answer:"""
-
 @app.route('/ask', methods=['GET', 'POST'])
 def ask_question():
     if request.method == 'POST':
+        print(db.table_info)
         question = request.form['question']
-        if pages:
-            print(pages)
-            # Initialize vector store only if pages are not empty
-            vectordb = Chroma.from_documents(
-                documents=pages,
-                embedding=embeddings,
-                persist_directory='docs/chroma'
-            )
-            vectordb.persist()
-
-            """
-            # Retrieval chain setup
-            retriever = vectordb.as_retriever()
-            qa = ConversationalRetrievalChain.from_llm(
-                llm_hub,
-                retriever=retriever,
-                combine_docs_chain_kwargs={"prompt": PromptTemplate.from_template(template)}
-            )
-            result = qa({"question": question})
-            answer = result.get('answer', 'No answer found.')
-            print(f"answer:{answer}")
-            """
-
-            # initialize vectordb retriever object
-            qa = RetrievalQA.from_chain_type(
-                llm=llm_hub,
-                chain_type="stuff",
-                retriever=vectordb.as_retriever(),
-            )
-
-            df = pd.read_csv("data.csv") # load employee_data.csv as dataframe
-            python = PythonAstREPLTool(locals={"df": df}) # set access of python_repl tool to the dataframe
-
-            # create calculator tool
-            calculator = LLMMathChain.from_llm(llm=llm_hub, verbose=True)
-
-            df_columns = df.columns.to_list() 
-
-            # prep the (tk policy) vectordb retriever, the python_repl(with df access) and langchain calculator as tools for the agent
-            tools = [
-                Tool(
-                    name = "Expense Records",
-                    func=qa.run,
-                    description=f"""
-                    Useful for when you need to answer questions about expense records. 
-                    """
-                ),                
-                Tool(
-                    name = "Expense Record Data",
-                    func=python.run,
-                    description=f"""
-                    Useful for when you need to answer questions about expense records stored in pandas dataframe 'df'. 
-                    Run python pandas operations on 'df' to help you get the right answer.
-                     'df' has the following columns: {df_columns}      
-                    """
-                ),
-                Tool(
-                    name = "Calculator",
-                    func=calculator.run,
-                    description = f"""
-                    useful for when you need to answer questions about math.
-                    """
-                )
-            ]
-
-            # change the value of the prefix argument in the initialize_agent function. This will overwrite the default prompt template of the zero shot agent type
-            agent_kwargs = {'prefix': f'You are friendly expense tracking assistant. You are tasked to assist the current user on questions related to expenses. You have access to the following tools:'}
-
-            # initialize the LLM agent
-            agent = initialize_agent(tools, 
-                                    llm_hub, 
-                                    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, 
-                                    verbose=True, 
-                                    agent_kwargs=agent_kwargs
-                                    )
-
-            response = agent.run(question)
-            print(f"response: {response}")
-
-            return render_template('index.html', question=question, answer=response)
-        else:
-            return "No data loaded from CSV", 400
+        prompt = QUERY.format(question=question)
+        response = db_chain.run(prompt)
+        print(f"response: {response}")
+        return render_template('index.html', question=prompt, answer=response)
 
     return redirect(url_for('index'))
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
+
+
+
