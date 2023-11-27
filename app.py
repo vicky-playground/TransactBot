97c514a52f09c70892bd968f8c830f72744a4e12
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-11-27 14:52:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,25 @@
-# Assuming this function exists in model.py and utilizes the created CSV
+from flask import Flask, render_template, request
+import sqlite3
+from datetime import datetime
+import pytz
 from model import llm_hub, embeddings
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Chroma
-
-from langchain.document_loaders import CSVLoader
-from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import RetrievalQA
-import os
 from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
 from langchain.chains import LLMMathChain
-
-import sqlite3
 from langchain.prompts import PromptTemplate
 
-def get_db_connection():
-    conn = sqlite3.connect('expenses.db')
-    conn.row_factory = sqlite3.Row
-    # Create the table with an auto-incrementing primary key
-    conn.execute("""CREATE TABLE IF NOT EXISTS expenses (
-        rowid INTEGER PRIMARY KEY AUTOINCREMENT,
-        date DATE,
-        item TEXT,
-        amount INTEGER
-    )""")
-    conn.commit()
-    return conn
+from datetime import datetime
 
-"""
-Building LLM Connected To Our Database
-"""
-
-conn = sqlite3.connect('records.db')
-
-# Create a cursor object to execute SQL commands
-cursor = conn.cursor()
-
-# Create the tasks table if it doesn't exist
-conn.execute("""CREATE TABLE IF NOT EXISTS expenses (
-    rowid INTEGER PRIMARY KEY AUTOINCREMENT,
-    date DATE,
-    item TEXT,
-    amount INTEGER
-)""")
-
-# Insert sample tasks into the tasks table
-data = [
-    ('2023-11-11', 'Groceries', 150),
-    ('2023-11-13', 'Utilities', 75),
-    ('2023-11-15', 'Internet', 50),
-    ('2023-11-18', 'Transport', 30),
-    ('2023-11-20', 'Eating Out', 45),
-    ('2023-11-22', 'Groceries', 160),
-    ('2023-11-25', 'Gym Membership', 25),
-    ('2023-12-18', 'Utilities', 80),
-    ('2023-12-20', 'Clothing', 100),
-    ('2023-12-23', 'Entertainment', 60),
-    ('2023-12-25', 'Internet', 50),
-    ('2023-12-28', 'Groceries', 120),
-    ('2023-12-30', 'Phone Bill', 40)
-]
-# Ensure your database table structure matches these columns (date, item, amount)
-cursor.executemany('INSERT INTO expenses (date, item, amount) VALUES (?, ?, ?)', data)
-
-# Commit the changes and close the connection
-conn.commit()
-conn.close()
 
 from langchain.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
 # connect to our database
-db = SQLDatabase.from_uri("sqlite:///records.db")
+db = SQLDatabase.from_uri("sqlite:///banking.db")
 
 # We can use db.table_info and check which sample rows are included:
 print(db.table_info)
@@ -81,110 +27,88 @@ print(db.table_info)
 # create the database chain
 db_chain = SQLDatabaseChain.from_llm(llm_hub, db, verbose=True)
 
+
 # Create db chain
 QUERY = """
-Given an input question, first create a syntactically correct PostgreSQL query to run, then look at the results of the query and return the answer.
+<s>[INST] 
+Given an input inquiry related to banking transactions, create a syntactically correct SQLite-compatible SQL query to run. Then, execute this query against the 'transactions' table and provide the answer. 
+
+Guidelines:
+- If no specific date/time is mentioned, include all transactions.
+- If a specific date/time period is mentioned, filter results using the current time zone: {time}.
+- Transaction includes both credit and debit transaction types when running SQL query (transaction_type = 'debit' AND 'credit') if no transaction type is specified by the input query.
+- When running SQL query, remember that in the 'transactions' table, the positive integer in the column 'amount' represents income/earnings (WHERE amount > 0), but the negative integer represents expense/cost no matter if the corresponding transaction_type is debit or credit (WHERE amount <= 0).
+- In your answer, indicate expenses as plain numbers without a negative sign.
+- If the SQL query result is [(None,)], then the relevant amount is $0
+- "Largest transaction" or "smallest transaction" should be understood as the transaction with the greatest absolute value (ORDER BY ABS(amount) DESC LIMIT 1;), regardless of whether it's an income (positive number) or an expense (negative number).
+
+<<SYS>>
 Use the following format:
-
-Query: {question}
-SQLQuery: SQL Query to run
+Query: Input inquiry
+SQLQuery: SQL query to run
 SQLResult: Result of the SQLQuery
-Answer: Final answer here
+Answer: Final answer here with brief explanation
 
-
+{inquiry}
+<</SYS>>
+[/INST]
 """
-
-question = "total amount?"
-prompt = QUERY.format(question=question)
-answer = db_chain.run(prompt)
-print(f"answer: ")
-
-from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+def get_db_connection():
+    conn = sqlite3.connect('banking.db', check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
     conn = get_db_connection()
-    error_message = None
-
-    if request.method == 'POST':
-        date = request.form['date']
-        item = request.form['item']
-        amount = request.form['amount']
-
-        # Insert new expense into the database
-        conn.execute('INSERT INTO expenses (date, item, amount) VALUES (?, ?, ?)', (date, item, amount))
-        conn.commit()
-        save_to_csv()  # Update CSV file after adding new expense
-        return redirect(url_for('index'))
-
-    # Fetch all expenses from the database
-    all_expenses = conn.execute('SELECT rowid, * FROM expenses ORDER BY date DESC').fetchall()
-    for expense in all_expenses:
-        print(expense['rowid'])  # This should print the rowid of each expense
-
-    conn.close()
-
-    return render_template('index.html', all_expenses=all_expenses)
-
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-    conn = get_db_connection()
-    expense = conn.execute('SELECT * FROM expenses WHERE rowid = ?', (id,)).fetchone()
-
-    if request.method == 'POST':
-        date = request.form['date']
-        item = request.form['item']
-        amount = request.form['amount']
-        conn.execute('UPDATE expenses SET date = ?, item = ?, amount = ? WHERE rowid = ?', (date, item, amount, id))
-        conn.commit()
-        save_to_csv()
-        return redirect(url_for('index'))
-
-    conn.close()
-    return render_template('edit.html', expense=expense)
-
-@app.route('/delete/<int:id>', methods=['POST'])
-def delete(id):
-    conn = get_db_connection()
-    conn.execute('DELETE FROM expenses WHERE rowid = ?', (id,))
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS transactions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date DATE,
+            description TEXT,
+            amount INTEGER, 
+            transaction_type TEXT
+        )
+    """)
+    # Insert sample data only if the table is empty
+    if conn.execute('SELECT COUNT(*) FROM transactions').fetchone()[0] == 0:
+        sample_data = [
+            ('2024-01-25', 'Salary', 2000, 'debit'),               # Income
+            ('2023-12-25', 'Salary', 2000, 'debit'),               # Income
+            ('2023-12-15', 'Christmas Shopping', -250, 'credit'), # Expense
+            ('2023-12-01', 'Electricity Bill', -150, 'debit'),   # Expense
+            ('2023-03-15', 'Car Maintenance', -300, 'credit'),    # Expense
+            ('2023-02-14', 'Valentine’s Gift', -100, 'debit'),   # Expense
+            ('2023-01-01', 'New Year Party', -200, 'credit')      # Expense            
+        ]
+        conn.executemany('INSERT INTO transactions (date, description, amount, transaction_type) VALUES (?, ?, ?, ?)', sample_data)
     conn.commit()
     conn.close()
-    return redirect(url_for('index'))
 
-@app.route('/records')
-def records():
+init_db()
+
+@app.route('/')
+def index():
     conn = get_db_connection()
-    expenses = conn.execute('SELECT rowid, * FROM expenses ORDER BY date DESC').fetchall()
-
-    # SQL to calculate total amount spent per month
-    totals_by_month = conn.execute("""
-        SELECT 
-            strftime('%Y-%m', date) as month, 
-            sum(amount) as total 
-        FROM expenses
-        GROUP BY strftime('%Y-%m', date)
-        ORDER BY month DESC
-    """).fetchall()
-
+    transactions = conn.execute('SELECT id, * FROM transactions ORDER BY date DESC').fetchall()
     conn.close()
-    return render_template('records.html', expenses=expenses, totals_by_month=totals_by_month)
+    return render_template('index.html', transactions=transactions)
 
-
-@app.route('/ask', methods=['POST'])
-def ask_question():
-    print(request.form)
-    print(db.table_info)
-    question = request.form['question']
-    print(f"Question received: {question}")
-    prompt = QUERY.format(question=question)
+@app.route('/inquiry', methods=['POST'])
+def submit_inquiry():
+    inquiry = request.form['inquiry']
+    prompt = QUERY.format(time=datetime.now(pytz.timezone('America/New_York')), inquiry=inquiry)
     response = db_chain.run(prompt)
     print(f"answer: {response}")
-    return render_template('index.html', question=prompt, answer=response)
+
+    # Fetch transactions data again to pass to the template
+    conn = get_db_connection()
+    cursor = conn.execute('SELECT id, * FROM transactions ORDER BY date DESC')
+    transactions = [dict(ix) for ix in cursor.fetchall()]
+    conn.close()
+    return render_template('index.html', inquiry=prompt, answer=response, transactions=transactions)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
