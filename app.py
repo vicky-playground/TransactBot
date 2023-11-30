09021983b83bcf80f83601c5dcc26d9ab5140e9c
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-11-30 18:30:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request
-import sqlite3
 from datetime import datetime
 import pytz
-from model import llm_hub, embeddings
 from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
 from langchain.chains import LLMMathChain
@@ -10,20 +8,9 @@ from langchain.prompts import PromptTemplate
 
 from datetime import datetime
 
-
-from langchain.utilities import SQLDatabase
-from langchain_experimental.sql import SQLDatabaseChain
-# connect to our database
-db = SQLDatabase.from_uri("sqlite:///history.db")
-
-# We can use db.table_info and check which sample rows are included:
-print(db.table_info)
-
-# create the database chain
-db_chain = SQLDatabaseChain.from_llm(llm_hub, db, verbose=True)
+from db import *
 
 
-# Create db chain
 QUERY = """
 <s>[INST] 
 You are a powerful text-to-SQLite model. Your job is to answer questions about a database. You are given a question and context regarding the table of credit card transaction records which represents a person's expense records. You must run SQLite queries to the table to find an answer. Ensure your query does not include any non-SQLite syntax such as DATE_TRUNC or any use of backticks (`) or "```sql". Then, execute this query against the 'transactions' table and provide the answer. 
@@ -58,59 +45,6 @@ Advice: Provide strategies to manage expenses or tips to reduce the expenses her
 
 
 app = Flask(__name__)
-
-def get_db_connection():
-    conn = sqlite3.connect('history.db', check_same_thread=False)
-    conn.row_factory = sqlite3.Row
-    return conn
-
-def init_db():
-    conn = get_db_connection()
-    conn.execute("""
-        CREATE TABLE IF NOT EXISTS transactions (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            date DATE,
-            category TEXT,
-            merchant TEXT,
-            amount INTEGER
-        );
-    """)
-    # Insert sample data only if the table is empty
-    if conn.execute('SELECT COUNT(*) FROM transactions').fetchone()[0] == 0:
-        sample_data = [
-            ('2023-12-05', 'Online Shopping', 'E-Store', 120),
-            ('2023-12-02', 'Utilities', 'Internet Bill', 80),
-            # Nov Transactions
-            ('2023-11-29', 'Dining', 'Restaurant A', 85),
-            ('2023-11-22', 'Groceries', 'Supermarket', 60),
-            ('2023-11-15', 'Travel', 'Airline Ticket', 350),
-            ('2023-11-12', 'Utilities', 'Electricity Bill', 75),
-            ('2023-11-08', 'Online Shopping', 'Online Marketplace', 45),
-            ('2023-11-05', 'Entertainment', 'Cinema', 30),
-            # Transactions from earlier in the year
-            ('2023-10-20', 'Dining', 'Cafe', 20),
-            ('2023-10-15', 'Travel', 'Hotel Booking', 200),
-            ('2023-10-10', 'Groceries', 'Local Market', 50),
-            ('2023-10-06', 'Utilities', 'Water Bill', 40),
-            ('2023-09-25', 'Online Shopping', 'Tech Store', 150),
-            ('2023-09-15', 'Travel', 'Taxi Service', 25),
-            ('2023-09-22', 'Dining', 'Restaurant B', 90),
-            ('2023-09-10', 'Utilities', 'Electricity Bill', 100),
-            ('2023-09-05', 'Groceries', 'Grocery Store', 80),
-            ('2023-08-18', 'Utilities', 'Gas Bill', 60),
-            ('2023-08-12', 'Entertainment', 'Streaming Service', 15),
-            ('2023-08-06', 'Online Shopping', 'E-Store', 300),
-            ('2023-07-30', 'Travel', 'Train Ticket', 40),
-            ('2023-07-10', 'Dining', 'Fast Food', 25),
-            ('2023-07-03', 'Utilities', 'Water Bill', 50)
-        ]
-
-        conn.executemany('INSERT INTO transactions (date, category, merchant, amount) VALUES (?, ?, ?, ?)', sample_data)
-    conn.commit()
-    conn.close()
-
-init_db()
-
 
 @app.route('/')
 def index():
