4df9a10a7eaa07dd4bf43c2c1f3b6cc20ad1bba9
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-11-30 23:07:13 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,10 @@
-from model import llm_hub, embeddings
 from langchain.utilities import SQLDatabase
-from langchain_experimental.sql import SQLDatabaseChain
 import sqlite3
 
-# connect to our database
+# Create our database
 db = SQLDatabase.from_uri("sqlite:///history.db")
 
-# create the database chain
-db_chain = SQLDatabaseChain.from_llm(llm_hub, db, verbose=True)
-
+# Function to connect to SQLite database
 def get_db_connection():
     conn = sqlite3.connect('history.db', check_same_thread=False)
     conn.row_factory = sqlite3.Row
@@ -16,6 +12,7 @@ def get_db_connection():
 
 def init_db():
     conn = get_db_connection()
+    # Create a table
     conn.execute("""
         CREATE TABLE IF NOT EXISTS transactions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -55,7 +52,10 @@ def init_db():
             ('2023-07-03', 'Utilities', 'Water Bill', 50)
         ]
 
+        # Insert sample data into the tasks table
         conn.executemany('INSERT INTO transactions (date, category, merchant, amount) VALUES (?, ?, ?, ?)', sample_data)
+        
+    # Commit the changes and close the connection
     conn.commit()
     conn.close()
 
@@ -63,3 +63,15 @@ init_db()
 
 # We can use db.table_info and check which sample rows are included:
 print(db.table_info)
+
+# Function to get table columns from SQLite database
+def get_table_columns(table_name):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("PRAGMA table_info({})".format(table_name))
+    columns = cursor.fetchall()
+    print(f"columns:{columns}")
+    return [column[1] for column in columns]
+
+table_name = 'transactions'
+columns = get_table_columns(table_name)
